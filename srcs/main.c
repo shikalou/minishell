90f5e485b8d7ed9a600896c58ed08fc311da9a4b
Marine COUPPE <mcouppe@e2r11p23.clusters.42paris.fr>
2022-05-24 14:07:07 +0200

    L'election encourage le charlatanisme - Ernest Renan

--- srcs/main.c
+++ srcs/main.c
@@ -1,10 +1,23 @@
 #include "minishell.h"
 
+int	ft_parsing(char *cmd)
+{
+	char	**splitted;
+
+	splitted = ft_split(cmd, ' ');
+	printf("%s\n", splitted[0]);
+	return (0);
+}
+
+
 int	main()
 {
 	char *cmd;
-
-	cmd = readline("nelson et zor minishell$ ");
-	printf("%s\n", cmd);
+	while (1)
+	{
+		cmd = readline("nelson et zor minishell$ ");
+		printf("%s\n", cmd);
+		ft_parsing(cmd);
+	}
 	return (0);
 }
