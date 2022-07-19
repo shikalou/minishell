eb2b361e28c7d6583529635731b3ad1605a5af01
Marine COUPPE <mcouppe@student.42.fr>
2022-07-19 22:10:57 +0200

    Un magicien n est jamais en retard, Frodon Sacquet. Pas plus qu il n est en avance. Il arrive precisement a l heure prevue - Gandalf

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 16:37:58 by ldinaut           #+#    #+#             */
-/*   Updated: 2022/07/19 18:12:35 by ldinaut          ###   ########.fr       */
+/*   Updated: 2022/07/19 21:16:13 by mcouppe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,9 @@ int	parsing_ending(char *cmd, t_big *big_s)
 	parsing_redirection(big_s, 0);
 	if (big_s->cmd_lst == NULL)
 		return (0);
+	printf("in parsing %s\n", big_s->input[0]);
+	if (ft_strnstr(big_s->input[0], "./minishell", ft_strlen(big_s->input[0])) != NULL)
+		ft_update_shlvl(big_s);
 	return (1);
 }
 
